a252ac9cabb6ab7ab6b043c7181f6e1dc8211c6c
h.oda <62193961+hiroic-works@users.noreply.github.com>
2022-02-18 20:32:04 +0900

    setup frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header class="bg-blue-500">
+      <nav class="text-white flex items-center justify-end">
+        <RouterLink to="/" class="m-2">Home</RouterLink>
+        <RouterLink to="/about" class="m-2">About</RouterLink>
+      </nav>
+  </header>
+
+  <RouterView />
+</template>
