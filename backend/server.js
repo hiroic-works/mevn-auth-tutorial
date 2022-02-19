ab1105ace5f946cf5fe13525375ee7824038fdec
h.oda <62193961+hiroic-works@users.noreply.github.com>
2022-02-19 21:24:31 +0900

    corsの対応

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const colors = require('colors')
+const cors = require('cors');
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
 const {errorHandler} = require('./middleware/errorMiddleware')
@@ -8,6 +9,12 @@ const connectDB = require('./config/db')
 connectDB()
 
 const app = express()
+
+const origins = process.env.Node_ENV === 'development' ? ['http://localhost:3000'] : ['http://exsample.com']
+app.use(cors({
+    origin: origins, //アクセス許可するオリジン
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}))
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
